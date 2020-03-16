871ce2328198be483b1603a114e12f3054df15af
bmyjacks <bmyjacks@outlook.com>
2020-03-16 21:16:47 +0800

    Update config

--- public/config.js
+++ public/config.js
@@ -2,7 +2,7 @@
 window.Config = {
 
   // 站点名
-  SiteName: 'Uptime Status',
+  SiteName: 'BMYJACKS Service Status',
 
   // 站点链接
   SiteUrl: '/',
@@ -10,14 +10,14 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'm784514358-420ca4f5c4f4ee92ff06a064',
+    'm784379265-9fd5d1878f815ef86e8c5162',
+    'm784514386-b3bde2a0c57c7e6737269fc1',
+    'm784379275-9dd5f08a54c09b167733009e',
   ],
 
   // 是否显示监测站点的链接
-  ShowLink: true,
+  ShowLink: false,
 
   // 日志天数
   // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
@@ -27,12 +27,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Status',
+      url: 'https://status.bmyjacks.cn/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
+      text: 'Blog',
+      url: 'https://www.bmyjacks.cn/'
     }
   ]
 };
