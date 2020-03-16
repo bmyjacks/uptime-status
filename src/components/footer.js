f7a9b030af00ac7bbbac4b483e8567881a0497eb
bmyjacks <bmyjacks@outlook.com>
2020-03-16 21:32:20 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="container">
         <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
         <p>&copy; 2020 <Link to="https://www.bmyjacks.cn/" text="BMYJACKS Group" /></p>
-        <p> 感谢 <a href="https://github.com/yb/uptime-status">yb/uptime-stats</a></p>
+        <p> 感谢 <Link to="https://github.com/yb/uptime-status" text="yb/uptime-status" /></p>
       </div>
     </div>
   );
