8c3bfccbaa1538641aeb74af6ff26a7728d8a758
Mingyi Bao <bmyjacks@outlook.com>
2021-01-01 07:19:48 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div id="footer">
       <div className="container">
         <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
-        <p>&copy; 2020 <Link to="https://www.bmyjacks.cn/" text="BMYJACKS Group" /></p>
+        <p>&copy; 2020-2021 <Link to="https://www.bmyjacks.cn/" text="BMYJACKS" /></p>
         <p> 感谢 <Link to="https://github.com/yb/uptime-status" text="yb/uptime-status" /></p>
       </div>
     </div>
